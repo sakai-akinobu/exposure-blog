f7b675dc0cd525f282f8a022eb2771be8347c1cd
sakai-akinobu <sakai.akinobu@gmail.com>
2019-09-23 12:08:22 +0900

    Add `font-family` property.

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,6 +58,7 @@ class Layout extends React.Component {
           marginRight: `auto`,
           maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          fontFamily: `-apple-system,BlinkMacSystemFont,Helvetica Neue,Segoe UI,Hiragino Kaku Gothic ProN,Hiragino Sans,ヒラギノ角ゴ ProN W3,Arial,メイリオ,Meiryo,sans-serif`,
         }}
       >
         <header>{header}</header>
