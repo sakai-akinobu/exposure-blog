47f4c92b6b3ddbcdbcf146a9a76e4423c5562011
sakai-akinobu <sakai.akinobu@gmail.com>
2019-09-24 21:38:53 +0900

    Fix image quality.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 1000,
+              quality: 70,
             },
           },
           {
